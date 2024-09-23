feb3f64d22a55e9d04a0ad848740634d1c606cd3
Magma73 <marine.magnin73@gmail.com>
2024-09-23 12:37:42 +0200

    feat: Add PencilIcon for task editing

--- src/app/_components/createtask.tsx
+++ src/app/_components/createtask.tsx
@@ -6,6 +6,7 @@ import { api } from "~/trpc/react";
 
 // import { FiEdit } from 'react-icons/fi';
 
+import { PencilIcon } from '@heroicons/react/24/outline';
 
 export default function CreateTask() {
 
@@ -108,9 +109,7 @@ export default function CreateTask() {
                             <li key={task.id} className="flex justify-between items-center gap-5 mt-4">
                                 <span className="overflow-hidden text-[hsl(280,100%,70%)] bg-white px-6 py-3 shadow rounded-md mr-4">{task.task}</span>
                                 <span onClick={() => handleEditTask(task)}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-[#CC66FF]" cursor="pointer">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                                    </svg>
+                                    <PencilIcon className="h-6 w-6 text-[#CC66FF] cursor-pointer" />
                                 </span>
                                 <button onClick={() => handleDelete(task.id)} className="bg-[hsl(280,43%,39%)] px-6 py-2.5 rounded-md">Supprimer</button>
                             </li>
