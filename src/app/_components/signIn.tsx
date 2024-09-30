87f382ea998e3cda04d19bca666769513d71328e
Magma73 <marine.magnin73@gmail.com>
2024-09-30 22:21:44 +0200

    feat: add new colors,  background gradient, floating animation to image and hover effect to arrow icon
    - Added new colors to Tailwind CSS configuration
    - Added background gradient to call-to-action button
    - Added floating animation to the image on hover
    - Applied hover effect to the arrow icon to bounce

--- src/app/_components/signIn.tsx
+++ src/app/_components/signIn.tsx
@@ -3,7 +3,7 @@ import { signIn } from "next-auth/react"
 
 export default function SignIn() {
     return (
-        <button className="bg-primary py-3 px-3 rounded-md shadow-md " onClick={() => signIn('discord', { callbackUrl: '/tasks' })}>
+        <button className="bg-gradient-to-r from-primaryGradient to-primary py-3 px-3 rounded-md shadow-lg hover:from-primaryGradient hover:to-primaryGradient hover:shadow-xl" onClick={() => signIn('discord', { callbackUrl: '/tasks' })}>
             Sign in with Discord
         </button>
     )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,25 @@ export default {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
       colors: {
-        primary: '#7360f1',    // Electric Lavender
-        secondary: '#2D7AF5',  // Ocean Blue
-        tertiary: '#81ECFF',   // Azure
+        primary: '#7360f1',
+        primaryGradient:'#8271F3',    // Electric Lavender
+        secondary: '#2D7AF5',
+        secondaryGradient:'#0062FF',  // Ocean Blue
+        tertiary: '#81ECFF',
+        tertiaryGradient:'#B6F4FF',   // Azure
         accent: '#D297FF',
+        accentGradient:'#E3BDFF',
         warning:'#F24E1E',     // Lilac or Soft Lavender
-      }
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-5px)' },
+        },
+      },
+      animation: {
+        float: 'float 3s ease-in-out infinite',
+      },
     },
   },
   plugins: [],
